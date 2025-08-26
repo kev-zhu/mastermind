5fce03e208ef5e0dbdcf96b0e5d56eb0eeecdaa6
Kevin Zhu <kevinzhu35@gmail.com>
2025-08-25 20:37:39 -0700

    unit test for Feedback class
    test all methods for Feedback to ensure proper response.

---
+++ tests/test_feedback.py
@@ -0,0 +1,24 @@
+import pytest
+from mastermind import Feedback
+
+@pytest.mark.parametrize("correct_num, correct_loc, code_length, perfect", [
+  (4, 4, 4, True),    #True - perfect case
+  (2, 2, 4, False),   #False - imperfect case
+  (1, 1, 4, False),   #False - imperfect case
+  (2, 1, 4, False),   #False - imperfect case
+  (0, 0, 4, False)    #False - imperfect case
+])
+def test_feedback_is_perfect(correct_num, correct_loc, code_length, perfect):
+  feedback = Feedback(correct_num, correct_loc, code_length)
+  assert feedback.is_perfect() == perfect
+
+@pytest.mark.parametrize("correct_num, correct_loc, code_length, feedback_string", [
+  (4, 4, 4, "4 correct numbers and 4 correct locations"),   #4nums/4locs
+  (2, 2, 4, "2 correct numbers and 2 correct locations"),   #2nums/2locs
+  (1, 1, 4, "1 correct number and 1 correct location"),     #1num/1loc
+  (2, 1, 4, "2 correct numbers and 1 correct location"),    #2nums/1loc
+  (0, 0, 4, "0 correct numbers and 0 correct locations")    #0nums/0locs
+])
+def test_feedback_to_string(correct_num, correct_loc, code_length, feedback_string):
+  feedback = Feedback(correct_num, correct_loc, code_length)
+  assert feedback.to_string() == feedback_string
